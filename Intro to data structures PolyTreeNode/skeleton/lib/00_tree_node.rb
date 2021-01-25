1b7aae17755c221f96ffeae27d7ea7827986e8ea
Benjamin <benjamin.eastman@gmail.com>
2021-01-25 10:40:50 -0500

    message

--- Intro to data structures PolyTreeNode/skeleton/lib/00_tree_node.rb
+++ Intro to data structures PolyTreeNode/skeleton/lib/00_tree_node.rb
@@ -1,21 +1,11 @@
 class PolyTreeNode
 
+    attr_reader :parent, :children, :value
+    
     def initialize(value)
         @value = value
         @parent = nil
         @children = []
-    end
-
-    def parent
-        @parent
-    end
-
-    def children
-        @children
-    end
-
-    def value
-        @value
     end
 
     def parent=(name)
@@ -25,8 +15,48 @@ class PolyTreeNode
         end
         
         @parent = name
-        name.children << self
+        if !@parent.nil?
+            name.children << self
+        end
     end
 
+    def add_child(child_node)
+        child_node.parent=(self)
+    end
+
+    def remove_child(child_node)
+        if @children.include?(child_node)
+            child_node.parent=(nil)
+        else 
+            raise "Node is not a child"
+        end
+    end
+
+    def dfs(target_value)
+        if @value == target_value
+            return self
+        else
+            self.children.each do |child| 
+                result = child.dfs(target_value)
+                return result if !result.nil?
+            end
+        end
+        nil
+    end
+
+    def bfs(target_value)
+        queue = [self]
+
+        while queue.length > 0
+            element = queue.shift
+            if element.value == target_value
+                return element
+            else
+                queue << element.children
+                queue.flatten
+            end
+        end
+        nil
+    end
 
 end
